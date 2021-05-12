2e8c9e2a9002a2674a8888a2b449f5af98d3a3c7
anmorales-hub <moralesand2018@gmail.com>
2021-05-12 14:16:25 -0400

    Task: update auth.js

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Authentication services started on port 3000');
+    console.log('Authentication service started on port 3000');
 });
 
 const users = [
@@ -20,9 +20,9 @@ const users = [
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 
-app.use=(bodyParser.json());
+app.use(bodyParser.json());
 
-const accessTokenSecret ='youraccesstokensecret';
+const accessTokenSecret = 'youraccesstokensecret';
 
 app.post('/login', (req, res) => {
     // Read username and password from request body
@@ -41,4 +41,61 @@ app.post('/login', (req, res) => {
     } else {
         res.send('Username or password incorrect');
     }
+});
+
+const refreshTokenSecret = 'yourrefreshtokensecrethere';
+const refreshTokens = [];
+
+app.post('/login', (req, res) => {
+    // read username and password from request body
+    const { username, password } = req.body;
+
+    // filter user from the users array by username and password
+    const user = users.find(u => { return u.username === username && u.password === password });
+
+    if (user) {
+        // generate an access token
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+        const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
+
+        refreshTokens.push(refreshToken);
+
+        res.json({
+            accessToken,
+            refreshToken
+        });
+    } else {
+        res.send('Username or password incorrect');
+    }
+});
+
+app.post('/token', (req, res) => {
+    const { token } = req.body;
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    if (!refreshTokens.includes(token)) {
+        return res.sendStatus(403);
+    }
+
+    jwt.verify(token, refreshTokenSecret, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+
+        res.json({
+            accessToken
+        });
+    });
+});
+
+app.post('/logout', (req, res) => {
+    const { token } = req.body;
+    refreshTokens = refreshTokens.filter(token => t !== token);
+
+    res.send("Logout successful");
 });
